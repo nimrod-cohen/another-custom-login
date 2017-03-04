42e1facbf0a01dc0d115b179ac53988a146c8a89
nimrod cohen <nimrod.cohen@gmail.com>
2017-03-04 23:28:04 +0200

    default login page is empty string, not false.

--- another-custom-login.php
+++ another-custom-login.php
@@ -35,11 +35,11 @@ class AnotherCustomLogin
 		add_filter( 'body_class',array($this,'addLoginClassToBody'));
 		add_filter( 'login_url', array($this,'getLoginUrl'), 10, 3 );
 	}
-	
+
 	public static function getSettings()
 	{
 		$settings = get_option('anculo_settings',false);
-		
+
 		return !$settings ? array(
 			"login_page" => false,
 			"ralins" => array(),
@@ -50,17 +50,17 @@ class AnotherCustomLogin
 	public static function getSetting($name)
 	{
 		$settings = self::getSettings();
-		
+
 		return $settings[$name];
 	}
 
 	public static function setSettings($args)
 	{
 		$settings = self::getSettings();
-		
+
 		foreach($args as $key => $val)
 			$settings[$key] = $val;
-		
+
 		update_option("anculo_settings",$settings);
 	}
 
@@ -70,11 +70,11 @@ class AnotherCustomLogin
 		$requestUrl = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"].parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 		return $loginUrl == $requestUrl;
 	}
-	
+
 	private function doLogin()
 	{
 		$user = wp_signon();
-		
+
 		if(is_wp_error($user))
 			$this->loginError = $user->get_error_message();
 		else if (isset($_REQUEST['redirect_to']))
@@ -90,9 +90,9 @@ class AnotherCustomLogin
 	{
 		$roles = $user->roles;
 		$role = array_shift($roles);
-		
+
 		$ralins = self::getSetting("ralins");
-		
+
 		if(isset($ralins[$role]) && strlen($ralins[$role]) > 0)
 			wp_redirect(get_page_link($ralins[$role]));
 		else if(user_can($user,"manage_options"))
@@ -101,7 +101,7 @@ class AnotherCustomLogin
 			wp_redirect(home_url());
 		exit;
 	}
-	
+
 	private function sendResetEmail($email,$key,$login)
 	{
 		$resetUrl = network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($login), 'login');
@@ -111,7 +111,7 @@ class AnotherCustomLogin
 		$message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
 		$message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
 		$message .= "<a href='" . $resetUrl . "'>".$resetUrl."</a>\r\n";
-		
+
 		if ( is_multisite() )
 			$blogname = $GLOBALS['current_site']->site_name;
 		else
@@ -120,42 +120,42 @@ class AnotherCustomLogin
 			 * in sanitize_option we want to reverse this for the plain text arena of emails.
 			 */
 			$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
-		
+
 		$title = sprintf( __('[%s] Password Reset'), $blogname );
-		
+
 		wp_mail( $email, wp_specialchars_decode( $title ), $message );
-		
+
 	}
-	
+
 	private function doLogout()
 	{
 		wp_logout();
 		wp_redirect(wp_login_url());
 	}
-	
+
 	private function doResetPassword()
 	{
 		$this->loginError = false;
 		$user = check_password_reset_key( $_REQUEST['key'], $_REQUEST['user_login'] );
-		
+
 		if(is_wp_error($user))
 		{
 			$this->loginError = $user->get_error_message();
 			return;
 		}
-		
+
 		$pass1 = isset($_POST["pass1"]) ? trim($_POST["pass1"]) : "";
 		$pass2 = isset($_POST["pass2"]) ? trim($_POST["pass2"]) : "";
-		
+
 		if(strlen($pass1) == 0)
 			$this->loginError = "Password is empty";
 		else if($pass1 != $pass2)
 			$this->loginError = "Passwords mismatch";
-		
+
 		reset_password($user, $pass1);
 		$this->loginError = "Password changed successfully";
 	}
-	
+
 	private function doLostPassword()
 	{
 		$user = false;
@@ -166,12 +166,12 @@ class AnotherCustomLogin
 			$this->loginError = "Invalid login or email address, or address does not exist";
 			return false;
 		}
-		
+
 		$user = get_user_by("email",$login);
-		
+
 		if(!$user)
 			$user = get_user_by("login",$login);
-		
+
 		if (!$user)
 		{
 			$this->loginError = "Invalid login or email address, or address does not exist";
@@ -184,29 +184,29 @@ class AnotherCustomLogin
 			$this->loginError = "Invalid login or email address, or address does not exist";
 			return false;
 		}
-		
+
 		$email = $user->user_email;
 
 		$this->sendResetEmail($email, $key,$user->user_login);
 		$this->loginError = "Check your email for a link to reset your password";
-		
+
 		return true;
 	}
-	
+
 	public function doLoginActions()
 	{
 		if($this->isLoginPage())
 		{
 			$this->loginError = false;
-			
+
 			$action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : null;
-			
+
 			if (is_user_logged_in() && $action != "logout")
 			{
 				$this->redirectLoggedInUser(wp_get_current_user());
 				return;
 			}
-			
+
 			switch ($action)
 			{
 				case "do_login":
@@ -228,30 +228,30 @@ class AnotherCustomLogin
 	public function protectWPLogin()
 	{
 		$loginPage = self::getSetting("login_page");
-		
-		if(strstr($_SERVER["REQUEST_URI"],"/wp-login.php") > -1 && false !== $loginPage)
+
+		if(strstr($_SERVER["REQUEST_URI"],"/wp-login.php") > -1 && "" !== $loginPage)
 		{
 			$link = get_page_link($loginPage).(!empty($_SERVER["QUERY_STRING"]) ? "?".$_SERVER["QUERY_STRING"] : "");
 			wp_redirect($link);
 			exit;
 		}
 	}
-	
+
 	public function renderManageScreen()
 	{
 		include_once("settings.php");
 	}
-	
+
 	public function addMenus()
 	{
 		add_options_page("Another Custom Login","Custom Login","manage_options","anculo-settings",array($this,"renderManageScreen"));
 	}
-	
+
 	//this happens after page already started to render.
 	public function showLogin($atts)
 	{
 		$action = isset($_GET["action"])? $_GET["action"] : "login";
-		
+
 		switch($action)
 		{
 			case "lostpassword":
@@ -271,28 +271,28 @@ class AnotherCustomLogin
 				return $this->getTemplate("login",$atts);
 		}
 	}
-	
+
 	private function getTemplate( $template_name, $attributes = null ) {
 		if ( ! $attributes ) {
 			$attributes = array();
 		}
-		
+
 		ob_start();
-		
+
 		require( 'templates/' . $template_name . '.php');
-		
+
 		$html = ob_get_contents();
 		ob_end_clean();
-		
+
 		return $html;
 	}
-	
+
 	public function enqueueLoginCSS()
 	{
 		if($this->isLoginPage())
 			wp_enqueue_style('login_css',admin_url("css/login.css"));
 	}
-	
+
 	public function addLoginClassToBody($classes)
 	{
 		if($this->isLoginPage())
@@ -303,7 +303,7 @@ class AnotherCustomLogin
 	public function getLoginUrl( $origUrl, $redirect = "", $force_reauth = false)
 	{
 		$login = self::getSetting("login_page");
-		
+
 		if($login && strlen($login) > 0)
 			$login = get_page_link($login);
 		else
